d4d7bd3aea4599c8792e9c3b8a5e698e28707278
AIME15 <eugenech@mit.edu>
2014-02-15 02:41:09 -0500

    login works

--- public/login.php
+++ public/login.php
@@ -26,7 +26,7 @@
             $row = $rows[0];
 
             // compare hash of user's input against hash that's in database
-            if (crypt($_POST["password"], $row["hash"]) == $row["hash"])
+            if ($_POST["password"] == $row["password"])
             {
                 // remember that user's now logged in by storing user's ID in session
                 $_SESSION["id"] = $row["id"];
