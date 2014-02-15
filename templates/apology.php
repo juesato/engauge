d6f69440088f3b08c20e292c3a74d33ba6a56b4d
Kai Xiao <kaix@mit.edu>
2014-02-15 05:22:38 -0500

    Changed some more registration/login stuff

--- templates/apology.php
+++ templates/apology.php
@@ -1,8 +1,35 @@
-<p class="lead text-danger">
-    Sorry!
-</p>
-<p class="text-danger">
-    <?= htmlspecialchars($message) ?>
-</p>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="../public/boot/favicon.ico">
 
-<a href="javascript:history.go(-1);">Back</a>
+        <title>Registration</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="../public/boot/bootstrap.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="../public/css/register.css" rel="stylesheet">
+        <link href="../public/css/styles.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+
+		<p class="lead_text_danger">
+		    Sorry!
+		</p>
+		<p class="text_danger">
+		    <?= htmlspecialchars($message) ?>
+		</p>
+
+		<a href="javascript:history.go(-1);">Back</a>
+
+	</body>
+
+</html>
