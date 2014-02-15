0c4ed762f8cfd9115d47e02d99ececfd5f841034
AIME15 <eugenech@mit.edu>
2014-02-14 22:21:18 -0500

    no more pset7

---
+++ templates/quote_form.php
@@ -0,0 +1,12 @@
+<form action="../public/quote.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autofocus class="form-control" name="symbol" placeholder="Ticker" type="text"/>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">Submit!</button>
+        </div>
+
+    </fieldset>
+</form>
