1af57abdd2fd4172f55ef2ee60db5d7d137cfc08
Jonathan Uesato <juesato@mit.edu>
2014-02-15 05:13:18 -0500

    prettifying

--- templates/classroom.php
+++ templates/classroom.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+
 <?php
 
 	if (array_key_exists('class_id', $_GET)) {
@@ -5,7 +7,10 @@
 		$_SESSION['class_id'] = $_GET["class_id"];
 	}
 
-	<form action="../public/add_question.php" method="post">
+?>
+
+
+<!-- <form action="../public/add_question.php" method="post">
     <fieldset>
         <div class="form-group">
             <input autofocus class="form-control" name="topic" placeholder="Topic" type="text"/>
@@ -20,6 +25,39 @@
         </div>
     </fieldset>
 </form>
+ -->
+<html>
+<body>
 
+<link rel="stylesheet" href="../public/boot/bootstrap.css"  type="text/css/">
+<link rel="stylesheet" href="../public/css/custom.css"  type="text/css/">
 
-?>
+	<div class="container">
+
+		<form action="../public/add_question.php" method="post">
+		    <fieldset>
+		        <div class="form-group col-lg-2">
+		            <input autofocus class="form-control width-full" name="topic" placeholder="Post Question Here" type="text"/>
+		        </div>
+
+		        <div class="form-group">
+		            <!-- <input class="form-control" name="question" placeholder="Question" type="text"/> -->
+		            <textarea class="form-control width-full" rows="8" name="question"/>
+
+		        </div>
+		        <div>
+					<label class="checkbox-inline">
+					  <input type="checkbox" id="inlineCheckbox1" value="anon"> Post Anonymously
+					</label>
+				</div>
+
+		        <div class="form-group">
+		            <button type="submit" class="btn btn-success">Submit</button>
+		        </div>
+		    </fieldset>
+		</form>
+
+	</div>1
+
+</body>
+</html>
