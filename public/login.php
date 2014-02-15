d1a9ff0e4632d62005deaa1892af9f5aa65932fd
Kai Xiao <kaix@mit.edu>
2014-02-15 03:31:27 -0500

    Fixed login

--- public/login.php
+++ public/login.php
@@ -26,11 +26,8 @@
             $row = $rows[0];
 
             // compare hash of user's input against hash that's in database
-<<<<<<< HEAD
             if (crypt($_POST["password"], $row["password"]) == $row["password"])
-=======
             if ($_POST["password"] == $row["password"])
->>>>>>> 7add64686facccc384bb9776d8537a7a15f7ac0a
             {
                 // remember that user's now logged in by storing user's ID in session
                 $_SESSION["id"] = $row["id"];
