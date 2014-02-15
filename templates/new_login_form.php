98c8d1fd9dd7bab074bdf67e981b164329c39220
Kai Xiao <kaix@mit.edu>
2014-02-15 03:45:55 -0500

    Made new login page look sightly nicer

--- templates/new_login_form.php
+++ templates/new_login_form.php
@@ -26,20 +26,43 @@
         <![endif]-->
     </head>
     <body>
-        <form action="../public/login.php" method="post">
-            <fieldset>
-                <div class="form-group">
-                    <input autofocus class="form-control" name="username" placeholder="Username" type="text"/>
-                </div>
-                <div class="form-group">
-                    <input class="form-control" name="password" placeholder="Password" type="password"/>
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-default">Log In</button>
-                </div>
-            </fieldset>
-        </form>
-        <div>
+<!--
+        <div class="container">
+
+            <form class="form-signin" role="form">
+                <h2 class="form-signin-heading">Please sign in</h2>
+                <input type="email" class="form-control" placeholder="Email address" required autofocus>
+                <input type="password" class="form-control" placeholder="Password" required>
+                <label class="checkbox">
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </form>
+
+        </div>
+-->
+
+        <div class = "container">
+
+            <form class="form-signin" role="form" action="../public/login.php" method="post">
+                <h2 class="form-signin-heading">Please sign in</h2>
+                <fieldset>
+                    <div class="form-group">
+                        <input autofocus class="form-control" name="username" placeholder="Username" type="text"/>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" name="password" placeholder="Password" type="password"/>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-lg btn-primary btn-block">Log In</button>
+                    </div>
+                </fieldset>
+            </form>
+        </div> <!-- /container -->
+
+
+
+        <div class = "footer-signin">
             or <a href="../public/register.php">register</a> for an account
         </div>
     </body>
