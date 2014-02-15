e0c24ee186ca7c55f0b02d95cf9d931f24b30d63
Jonathan Uesato <juesato@mit.edu>
2014-02-15 13:12:33 -0500

    looks better

--- templates/classroom.php
+++ templates/classroom.php
@@ -31,7 +31,7 @@
 <html>
 <body>
 
-
+<link rel="stylesheet" href="../public/css/bootstrap.css" type="text/css/">
 <link rel="stylesheet" href="../public/boot/bootstrap.css"  type="text/css/">
 <link rel="stylesheet" href="../public/css/custom.css"  type="text/css/">
 
@@ -39,18 +39,21 @@
 <script src="../public/js/bootstrap.js"></script>
 
 	<div class="container">
+
+
 <script>
-var bootstrap_enabled = (typeof $().modal == 'function');
-console.log("TEST");
-console.log(bootstrap_enabled);
+// var bootstrap_enabled = (typeof $().modal == 'function');
+// console.log("TEST");
+// console.log(bootstrap_enabled);
 </script>
 
-		<div class="panel-group" id="accordion">
+
+<div class="panel-group" id="accordion">
   <div class="panel panel-default">
     <div class="panel-heading">
       <h4 class="panel-title">
         <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-          Collapsible Group Item #1
+          eugenesucks123
         </a>
       </h4>
     </div>
@@ -58,38 +61,35 @@ console.log(bootstrap_enabled);
       <div class="panel-body">
         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
       </div>
+
+
+	<button type="button" class="btn btn-mini btn-info" data-toggle="collapse" data-target="#demo">
+	  Show Answers
+	</button>
+
+	<div id="demo" class="collapse">abracadabra</div>
+
+
     </div>
   </div>
+</div>
+<div class="panel-group" id="accordion">
   <div class="panel panel-default">
     <div class="panel-heading">
       <h4 class="panel-title">
         <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-          Collapsible Group Item #2
+          jon is the best.
         </a>
       </h4>
     </div>
-    <div id="collapseTwo" class="panel-collapse collapse">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
-          Collapsible Group Item #3
-        </a>
-      </h4>
-    </div>
-    <div id="collapseThree" class="panel-collapse collapse">
+    <div id="collapseTwo" class="panel-collapse collapse in">
       <div class="panel-body">
         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
       </div>
     </div>
   </div>
 </div>
-
+  
 	      		<?php
 					$rows = query("SELECT * FROM questions WHERE class_id=  {$_SESSION['class_id']} ");
 					// dump($rows);
@@ -132,7 +132,7 @@ console.log(bootstrap_enabled);
 		    </fieldset>
 		</form>
 
-	</div>1
+	</div>
 
 </body>
 </html>
