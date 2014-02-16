fe2d30ddf670b7f21abe988c86c41791f4bdd29a
AIME15 <eugenech@mit.edu>
2014-02-16 04:13:04 -0500

    aldjfhvsjhfdlkjghlbjhglsfdjhvjk fuck you

--- public/receive_text.php
+++ public/receive_text.php
@@ -10,6 +10,15 @@ $sms = new NexmoMessage('1c7512a7', 'd77fd951');
     if ($sms->inboundText()) {
          //$sms->reply('You said: ' . $sms->text);
     	$raw_text = $sms->text;
+
+    	$number = $sms->from;
+
+    	// $pieces = explode(" ", $raw_text, 2);
+
+    	// if (count($pieces) == 2)
+    	// {
+    	// 	query("INSERT INTO questions (class_id, asker_id, topic, datetime, phone_reply) VALUES (?, ?, ?, ?, ?, ?)", )
+    	// }
 	}
 
 ?>
